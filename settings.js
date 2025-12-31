996f353d5c2b5ce0512478ce97ae87f01ea2c631
Korynthian <korynthian.m@gmail.com>
2025-12-30 23:55:42 -0500

    Added settings page; reorganized css

--- settings.js
+++ settings.js
@@ -1,25 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const darkModeToggle = document.getElementById('darkModeToggle');
-  const body = document.body;
+function confirmCustomCSS() {
+  document.querySelectorAll('style, link[rel="stylesheet"]').forEach(style => style.remove());
+  const style = document.createElement('style');
+  if (location.href.includes('/settings.html') && customCSS) {
+    const customCSS = document.getElementById('customCSS');
+    localStorage.setItem('CSS', customCSS.value);
+    style.textContent = customCSS.value;
+  }
+  else if (localStorage.getItem('CSS')) {
+    style.textContent = localStorage.getItem('CSS');
+  }
+  else {
+    window.open('settings.html', 'popupWindow');
+  }
+  document.head.appendChild(style);
+}
 
-  // Apply saved theme
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'light') {
-    body.classList.add('light-mode');
-    darkModeToggle.checked = true;
-  } else {
-    body.classList.remove('light-mode');
-    darkModeToggle.checked = false;
+document.addEventListener("DOMContentLoaded", async () => {
+  const confirmButton = document.getElementById('confirmCustomCSS');
+  if(confirmButton) {
+    if (localStorage.getItem('CSS')) {
+      document.getElementById('customCSS').value = localStorage.getItem('CSS');
+    }
+    confirmButton.addEventListener('click', confirmCustomCSS);
+    document.getElementById("darkDefaultThemeButton").addEventListener('click', () => {
+      fetch('/themes/default-dark.css')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('customCSS').value = data;
+          confirmCustomCSS();
+        })
+    });
+    document.getElementById("lightDefaultThemeButton").addEventListener('click', () => {
+      fetch('/themes/default-light.css')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('customCSS').value = data;
+          confirmCustomCSS();
+        })
+    });
+    document.getElementById("noThemeButton").addEventListener('click', () => {
+      document.getElementById('customCSS').value = "//unstyled theme"
+      confirmCustomCSS();
+    });
+    document.getElementById("changeUsername").addEventListener('click', () => {
+      const username = prompt("Please enter your username:");
+      localStorage.setItem('username', username || "Secret");
+    });
   }
 
-  // Dark mode toggle
-  darkModeToggle.addEventListener('change', function () {
-    if (this.checked) {
-      body.classList.add('light-mode');
-      localStorage.setItem('theme', 'light');
-    } else {
-      body.classList.remove('light-mode');
-      localStorage.setItem('theme', 'dark');
-    }
-  });
+  confirmCustomCSS();
 });
